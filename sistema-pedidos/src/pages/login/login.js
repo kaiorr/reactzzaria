a5927a50809faedbac2bd72d7f71c09548065d77
kaiorr <kaio.rocharibeiro@gmail.com>
2020-12-20 12:50:14 -0300

    Sistema-pedidos :: update :: Iniciando marcaçao da tela de login.

--- sistema-pedidos/src/pages/login/login.js
+++ sistema-pedidos/src/pages/login/login.js
@@ -1,9 +1,39 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+import styled from 'styled-components'
+import {Button, Grid} from '@material-ui/core'
+import GitHubIcon from '@material-ui/icons/GitHub';
+import { ReactComponent as Logo } from '../../assets/logo-react-zzaria.svg'
 
 const Login = () => (
-  <Fragment>
-    <h1>Login</h1>
-  </Fragment>
+  <Container>
+    <Grid container direction='column' justify='center' spacing={40}>
+      <Grid item>
+        <Logo style={{ width: '100%' }} />
+      </Grid>
+      <Grid item xs={12} container justify='center'>
+        <GitHubButton>
+          Logar com <GitHubIcon style={{ marginLeft: '5px' }} />
+        </GitHubButton>
+      </Grid>
+    </Grid>
+  </Container>
 )
+
+const Container = styled.div`
+  padding: 20px;
+`
+
+const GitHubButton = styled(Button).attrs({
+  variant: 'contained',
+  fullWidth: true
+})`
+  && {
+    max-width: 430px;
+    font-size: 22px;
+    padding: 5px;
+    text-transform: none;
+    margin-top: 20px;
+  }
+`
 
 export default Login
